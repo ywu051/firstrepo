29a951ebb71d305404eefc1909da945e4d670723
Yikai Wu <ywu051@hammer.cs.ucr.edu>
2018-01-17 18:37:07 -0800

    fixed the return 1 bug

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-#include <iostream> int main(){ return 0; }
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    std::cout << What is your name? << std::endl;
+    std::cin >> name;
+
+    return 0;
+}
+
