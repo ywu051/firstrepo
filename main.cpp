da6d1795da1cb4764e9ed592137de2b6860d666b
ywu051 <35508909+ywu051@users.noreply.github.com>
2018-01-17 17:48:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
+#include <iostream>
+
 int main()
 {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
-
+    std::cout << "hello git" << std::endl;
     return 1;
 }
